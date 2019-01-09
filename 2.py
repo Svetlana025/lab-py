0978669aeb53a87b3a8b436dd0197c1d8fd285fb
Svetlana025 <44404399+Svetlana025@users.noreply.github.com>
2019-01-10 00:58:28 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -9,14 +9,14 @@ if number == 1:
     if a == 0 and x == 0:
         print("\nПолучается деление на ноль.")
     else:
-        G = 0 - (3*(14*a**2 + 23*a*x - 30*x**2)) / ((0-(9*a**2)) + 37*a*x + 40*x**2)
+        G = -(3*(14*(a*2)+23*a*x-30*(x*2)))/-9*(a*2)+37*a*x+40*(x*2)
         print("\nG = ", G)
 elif number == 2:
     # Вывод F
     a = float(input("Введите число a: "))
     x = float(input("\nВведите число x: "))
     if (a >= -4 and a <= 4) and (x >= -4 and x <= 4):
-        F = math.0 - tan(18*a**2 - a*x - 4*x**2)
+        F = -tan(18*(a*2)-a*x-4*(x*2))
         print("\nF = ", F)
     else:
         print("\nЧисло слишком большое!")
@@ -27,7 +27,7 @@ elif number == 3:
     if a!=0 and x !=0:
         print("\nНевозможно сосчитать функцию. Работает только при a=0 и x=0")
     else:
-        Y = math.(log(35*a**2 - 27*a*x + 4*x**2 +1))/log(2)
+        Y = log(35*(a*2)-27*a*x+4*(x*2)+1)/log(2)
         print("\nY = ", Y)
 else:
     # Ошибочная ветвь
