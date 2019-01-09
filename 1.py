c007528e4aafd463c8c45fe89e24d9411282d635
Svetlana025 <44404399+Svetlana025@users.noreply.github.com>
2019-01-10 00:17:28 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,13 @@
-import math
+from math import sinh
+from math import atanh
 
-# Вывод G 
-a = float(input("Введите число a: "))
-x = float(input("\nВведите число x: "))
-G = 0 - (3*(14*a**2 + 23*a*x - 30*x**2)) / ((0-(9*a**2)) + 37*a*x + 40*x**2)
-print("\nG = ", G)
+a = float (input ("a:"))
+x = float (input ("x:"))
 
-# Вывод F
-a = float(input("Введите число a: "))
-x = float(input("\nВведите число x: "))
-F = math.0 - tan(18*a**2 - a*x - 4*x**2)
-print("\nF = ", F)
+G = -(3*(14*(a*2)+23*a*x-30*(x*2)))/(-9*(a*2))+37*a*x+40*(x*2))
+F = -tan(18*(a*2)-a*x-4*(x*2))
+Y = (log(35*(a*2)-27*a*x+4*(x*2)+1))/(log(2))
 
-# Вывод Y
-a = float(input("Введите число a: "))
-x = float(input("\nВведите число x: "))
-Y = math.(log(35*a**2 - 27*a*x + 4*x**2 +1))/log(2)
-print("\nY = ", Y)
+print ("G=""%2f" %G)
+print ("F=""%2f" %F)
+print ("Y=""%2f" %Y)
